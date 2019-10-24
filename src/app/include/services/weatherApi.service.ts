df8d3c42ebbfccbe0a5cffb766950871b8b28c29
xlord <xlordt@gmail.com>
2019-10-24 13:03:22 -0400

    Commit from Visual Stuio 2019

--- src/app/include/services/weatherApi.service.ts
+++ src/app/include/services/weatherApi.service.ts
@@ -1,18 +1,16 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
-
 @Injectable({
   providedIn: 'root'
 })
-
 
 export class WeatherApiService {
 
   // Endpoint to access AccuWeather.
   private endpoint = 'https://dataservice.accuweather.com';
 
-  // Weather service apikey - key will be removed, please with your own.
+  // Weather service apikey - key will be removed, please use with your own.
   private apiKey = 'eumva2hriurffm6T9U3ivQVxKtJhp8ep';
 
   // @param httpClient
